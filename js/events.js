eddd1abb6d2fb6c3ca023e0a2cde040ea2d7809d
Mike Pekala <mpekala411@gmail.com>
2018-01-21 18:21:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -9,6 +9,10 @@ function submitIt(){
   $('form').on('submit',alert("Your form is going to be submitted now."))
 }
 
+function pressit(){
+
+
+}
 //define functions here
 
 $(document).ready(function(){
