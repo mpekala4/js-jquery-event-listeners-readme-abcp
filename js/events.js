c621aa4ab1ada6f8d06c17465e76fd8de550443e
Mike Pekala <mpekala411@gmail.com>
2018-01-21 18:17:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,10 +8,19 @@ function frameIt(){
 function submitIt(){
   $('form').on('submit',alert("Your form is going to be submitted now."))
 }
+
+function pressIt(){
+  if (true) {
+    $('#71').on('keydown',alert())
+
+  } else $("#71").keydown(function() {
+    if (.keydown === 71 ) {.preventDefault();. }
+
+  } 
+}
 //define functions here
 
 $(document).ready(function(){
-
 // call functions here
 
 });
