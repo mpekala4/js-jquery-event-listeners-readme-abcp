a15d58f54c6179030e9d7b1042b0b42eab307f5c
Mike Pekala <mpekala411@gmail.com>
2018-01-21 18:06:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,6 +4,10 @@ function getIt(){
 function frameIt(){
   $('img').on('load',attr('tasty'))
 }
+
+function submitIt(){
+  $('form').on('submit',alert("Your form is going to be submitted now."))
+}
 //define functions here
 
 $(document).ready(function(){
